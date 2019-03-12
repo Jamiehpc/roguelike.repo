7d287aaa8ef7fb0c5426e6036fec0886c520fa2d
James COX <t7034151@linux15.scm.tees.ac.uk>
2019-03-12 10:12:24 +0000

    added dungeon calss

---
+++ include/Dungeon.h
@@ -0,0 +1,16 @@
+#ifndef DUNGEON_H
+#define DUNGEON_H
+
+
+class Dungeon
+{
+    public:
+        Dungeon();
+        virtual ~Dungeon();
+
+    protected:
+
+    private:
+};
+
+#endif // DUNGEON_H
